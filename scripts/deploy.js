04948d8b24c5431715a580e2dcaf9c0a64a19e04
masangri <masangri.art@gmail.com>
2022-06-05 11:42:44 -0700

    Update scripts/deploy.js Simplify and use env vars.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,39 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
 //
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+//`yarn hardhat run scripts/deploy.js`
 //
 
 
 require('dotenv').config();
 
-const { ethers, hre } = require("hardhat");
+const hre = require("hardhat");
 
-const PROJECT_NAME = 'ERC1155TestProject';
-const PROJECT_SYMBOL = 'TEST';
-const ROYALTY_FRACTION = 0;
+
+const PROJECT_NAME = process.env.PROJECT_NAME;
+const PROJECT_SYMBOL = process.env.PROJECT_SYMBOL;
+const ROYALTY_FRACTION = process.env.ROYALTY_FRACTION;
 
 
 async function main() {
   //
-
-  // We get the contract to deploy
-  const provider = ethers.getDefaultProvider();
-  const ownerWallet = new ethers.Wallet(process.env.TESTNET_PRIVATE_KEY, provider);
-
-  //
-  const [owner, guest] = await ethers.getSigners();
-
-  //
-  console.log(await ownerWallet.getAddress());
-  console.log(owner.address, guest.address);
-
-  //
-  const ProjectContract = await ethers.getContractFactory(PROJECT_NAME);
+  const ProjectContract = await hre.ethers.getContractFactory(PROJECT_NAME);
   const projectContract = await ProjectContract.deploy(PROJECT_NAME, PROJECT_SYMBOL, ROYALTY_FRACTION);
   await projectContract.deployed();
 
+  console.log(`"${PROJECT_NAME}" contract deployed successfully to ${projectContract.address} on the "${hre.network.name}" network!`);
 }
 
 
